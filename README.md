63fa710db1eafa7941df01bfc75e7d557d69fb8d
Matthew Gehrt <matthewagehrt@gmail.com>
2023-09-30 20:49:40 -0500

    Finished

--- README.md
+++ README.md
@@ -2,15 +2,97 @@
 
 ## Instructions for running locally
 
-1. Make sure you have Node.js installed
+1. Make sure you have Node.js installed. You can download it from [here](https://nodejs.org/).
 
-2. Clone this repository to your local machine
+2. Clone this repository to your local machine.
 
-3. Open the repository in the terminal and run the following commands
+3. Open the repository in a terminal and run the following command to install dependencies:
 
 ```
 npm install
+```
+
+5. Start the server by running:
+
+```
 node app.js
 ```
 
-4. Test the API as you wish, it will be running on port 8000
+This command will start the server, and it will be running on port 8000.
+
+4. Test the API as you wish, using tools like [Postman](https://www.postman.com/) or by sending HTTP requests directly. Refer to the "API Documentation" section below for details on available endpoints and usage examples.
+
+## API Documentation
+
+### Add Points
+
+**Endpoint**: POST `/add`
+
+Description: Adds points to the user's account.
+
+#### Example Usage:
+
+**Body:**
+
+```json
+{
+    "payer": "DANNON",
+    "points": 5000,
+    "timestamp": "2020-11-02T14:00:00Z"
+}
+```
+
+**Response**:
+
+```json
+N/A
+```
+
+### Spend Points
+
+**Endpoint**: POST `/spend`
+
+Description: Allows the user to spend points following specific rules.
+
+#### Example Usage:
+
+**Body**:
+
+```json
+{
+    "points": 2000
+}
+```
+
+**Response**:
+
+```json
+[
+    { "payer": "DANNON", "points": -1000 },
+    { "payer": "UNILEVER", "points": -1000 }
+]
+```
+
+### Get Points Balance
+
+**Endpoint**: GET `/balance`
+
+Description: Retrieves the user's point balance by payer.
+
+#### Example Usage:
+
+**Body**:
+
+```json
+N/A
+```
+
+**Response**:
+
+```json
+{
+    "DANNON": 4000,
+    "UNILEVER": 3000,
+    "MILLER COORS": 1000
+}
+```
--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { PriorityQueue } = require('@datastructures-js/priority-queue');
+
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.json());
 
-const pointsBalance = {};
-const transactions = [];
+const pointsBalance = {}; // Keep track of the user's total points by payer
+let pointsTotal = 0; // Keep track of the user's total points
+// Use a Priority Queue to keep the tranasctions sorted by date, so the oldest can easily be accessed
+const transactions = new PriorityQueue((a, b) => {
+    const timestampA = Date.parse(a.timestamp);
+    const timestampB = Date.parse(b.timestamp);
+    return timestampA - timestampB;
+});
 
 app.post('/add', (req, res) => {
     const { payer, points, timestamp } = req.body;
@@ -16,18 +24,15 @@ app.post('/add', (req, res) => {
         return res.status(400).send('Invalid request data');
     }
 
-    // Cannot add negative points
-    if (points <= 0) {
-        return res.status(400).send('Invalid request data, points must be positive');
-    }
+    transactions.enqueue({ payer, points, timestamp });
 
-    transactions.push({ payer, points, timestamp });
-
+    // Initialize payer points to 0 if it is a new payer
     if (!pointsBalance[payer]) {
         pointsBalance[payer] = 0;
     }
 
     pointsBalance[payer] += points;
+    pointsTotal += points;
 
     res.sendStatus(200);
 });
@@ -35,41 +40,60 @@ app.post('/add', (req, res) => {
 app.post('/spend', (req, res) => {
     const { points } = req.body;
 
+    // Make sure that all needed parameters were passed
     if (!points) {
         return res.status(400).send('Invalid request data');
     }
 
-    let pointsToSpend = points;
-    const spentPoints = [];
-
-    transactions.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-
-    for (const transaction of transactions) {
-        if (pointsToSpend <= 0) {
-            break;
-        }
-
-        const { payer, points: transactionPoints } = transaction;
-
-        if (transactionPoints >= 0) {
-            continue;
-        }
-
-        if (Math.abs(transactionPoints) >= pointsToSpend) {
-            spentPoints.push({ payer, points: -pointsToSpend });
-            pointsBalance[payer] += pointsToSpend;
-            pointsToSpend = 0;
-        } else {
-            spentPoints.push({ payer, points: transactionPoints });
-            pointsBalance[payer] += Math.abs(transactionPoints);
-            pointsToSpend -= Math.abs(transactionPoints);
-        }
-    }
-
-    if (pointsToSpend > 0) {
+    // The user doesn't have enough points if they try to spend more than they have
+    if (points > pointsTotal) {
         return res.status(400).send('Not enough points to spend');
     }
 
+    let pointsToSpend = points; // Number of points that still need to be spent
+    const spentPoints = []; // Points spent so far, to be returned
+
+    // Iterate until all transactions have been considered, or enough points have been spent
+    while (!transactions.isEmpty() && pointsToSpend > 0) {
+        const { payer, points: transactionPoints } = transactions.front();
+
+        // Find the index of the payer in the spentPoints array, if it exists
+        const existingPayerIndex = spentPoints.findIndex(obj => obj.payer === payer)
+
+        if (Math.abs(transactionPoints) > pointsToSpend) {
+            // Case where the oldest transaction has more than the amount of points needed to be spent
+            transactions.front().points -= pointsToSpend;
+
+            // Update the existing payer's spent points or add a new payer to spentPoints
+            if (existingPayerIndex !== - 1) {
+                spentPoints[existingPayerIndex].points -= pointsToSpend;
+            } else {
+                spentPoints.push({ payer, points: -pointsToSpend });
+            }
+
+            // Update payer's balance, total points, and point to spend 
+            pointsBalance[payer] -= pointsToSpend;
+            pointsTotal -= pointsToSpend;
+            pointsToSpend = 0; // All points spent
+        } else {
+            // Case where the oldest transaction has less than or equal the amount of points needed to be spent
+            transactions.dequeue();
+
+            // Update the existing payer's spent points or add a new payer to spentPoints
+            if (existingPayerIndex !== -1) {
+                spentPoints[existingPayerIndex].points -= transactionPoints;
+            } else {
+                spentPoints.push({ payer, points: -transactionPoints });
+            }
+
+            // Update payer's balance, total points, and point to spend 
+            pointsBalance[payer] -= transactionPoints;
+            pointsTotal -= transactionPoints;
+            pointsToSpend -= transactionPoints;
+        }
+    }
+
+    // Respond with the list of points spent for each payer
     res.json(spentPoints);
 });
 
